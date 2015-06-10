363e77c766634c87181e4de1df425bda0dfa36db
benwu95 <benwu95@gmail.com>
2015-06-10 22:24:11 +0800

    add SoundEffect.java

---
+++ src/SoundEffect.java
@@ -0,0 +1,55 @@
+// SoundEffect.init();
+// SoundEffect.volume = SoundEffect.Volume.ON;
+// JButton btn = new JButton("explode");
+// btn.addActionListener(new ActionListener()) {
+//     @Override
+//     public void actionPerformed(ActionEvent e) {
+//         SoundEffect.EXPLODE.play();
+//     }
+// }
+import java.io.File;
+import java.io.IOException;
+
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.AudioInputStream;
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.LineUnavailableException;
+
+public enum SoundEffect {
+    EXPLODE("sound/explode.wav"),
+    BEFOREBATTLE("sound/beforeBattle.wav");
+
+    public static enum Volume {
+        MUTE, ON
+    }
+    public static Volume volume = Volume.ON;
+
+    private Clip clip;
+
+    SoundEffect(String fileName) {
+        try {
+            File file = new File(fileName);
+            AudioInputStream audio = AudioSystem.getAudioInputStream(file);
+            clip = AudioSystem.getClip();
+            clip.open(audio);
+        } catch (UnsupportedAudioFileException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (LineUnavailableException e) {
+            e.printStackTrace();
+        }
+    }
+    public void play() {
+        if (volume != Volume.MUTE) {
+            if (clip.isRunning())
+                clip.stop();
+            clip.setFramePosition(0);
+            clip.start();
+        }
+    }
+    static void init() {
+        values(); //call the constructor for all the elements
+    }
+}
